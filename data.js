69da511498a1e8e55418f24d683eb851c32b380d
Chinmay <chinmaykotkars@gmail.com>
2024-11-19 20:14:29 +0530

    added local code files

---
+++ data.js
@@ -0,0 +1,215 @@
+export const bio = [
+  
+"Hello! I'm Chinmay Kotkar, A dedicated and results-oriented AI & DS Engineering student with a strong foundation in technical skills. An effective communicator and leader who excels in teamwork settings, fostering a collaborative and productive environment.",
+  "I thrive on solving complex problems, learning new tools, and contributing to impactful projects. My experiences include developing educational platforms, chatbots, data analysis tools, and more, reflecting my versatility and eagerness to explore new challenges. Beyond coding, I enjoy working in collaborative environments, sharing knowledge, and creating meaningful digital experiences.",
+
+"Feel free to explore my portfolio to learn more about my work and journey!"
+];
+
+export const skills = [
+  {
+    title: "Languages",
+    skillName: "C++, Python",
+    color: "1",
+    percentage: "80",
+  },
+  {
+    title: "Frameworks/Libraries",
+    skillName: "Pandas, Django, Scikit-Learn, Keras, Matplotlib",
+    color: "6",
+    percentage: "70",
+  },
+  // {
+  //   title: "Design",
+  //   skillName: "HTML, Bootstrap, Tailwind",
+  //   color: "4",
+  //   percentage: "70",
+  // },
+  {
+    title: "Version Control",
+    skillName: "GitHub, Git",
+    color: "7",
+    percentage: "70",
+  },
+  {
+    title: "Tools",
+    skillName: "Debugging, VS Code, MS Office, Google workspace ",
+    color: "3",
+    percentage: "80",
+  },
+//   {
+//     title: "Analytics",
+//     skillName: "CleverTap, Countly",
+//     color: "5",
+//     percentage: "50",
+//   },
+//   {
+//     title: "IDE",
+//     skillName: "VS Code",
+//     color: "6",
+//     percentage: "70",
+//   },
+];
+
+export const projects = {
+  webProjects: [
+    {
+      projectName: "HealthLM",
+      image: "images/rag.jpeg",
+      summary:
+        "[Ongoing] HealthLM-  Advanced chatbot that combines AI and your documents to deliver comprehensive and accurate responses ",
+      preview: "https://github.com/Chinmay-03",
+      techStack: ["Python","Langchain","Vector Database","Gen AI"],
+    },
+    {
+      projectName: "RAG-Powered Document Chatbot",
+      image: "images/rag.jpeg",
+      summary:
+        "Developed a chatbot utilizing a Retrieval-Augmented Generation (RAG) architecture, integrating OpenAI’s API with a vector database for document retrieval",
+      preview: "https://github.com/Chinmay-03",
+      techStack: ["Python", "Open AI API", "Vector DB"],
+    },
+    {
+      projectName: "Multilanguage Invoice extracter",
+      image: "images/invoice.png",
+      summary:
+        "Multilanguage Invoice Extractor- A web application that uses Google Gemini AI to extract and analyze invoice details in multiple languages. ",
+      preview: "https://github.com/Chinmay-03/Invoice-chat",
+      techStack: ["Python","Streamlit","Gemini API","Gen AI"],
+    },
+    
+   
+    
+  ],
+  softwareProjects: [
+    {
+      projectName: "Hackernews-Dashboard",
+      image: "images/hacknews.png",
+      summary:"Hackernews-Dashboard- This is a Streamlit application that fetches and displays job postings and top stories from Hacker News.",
+      preview: "https://jobsandstoriesonhackernews.streamlit.app/",
+      techStack: ["Python","Streamlit","Hacker News API"],
+    },
+    {
+      projectName: "Shorty - Link Shortener",
+      image: "images/shorty.png",
+      summary:"Shorty - Link Shortener- A lightweight link shortener service built with Node.js, designed for simplicity and efficiency. It is hosted on GitHub Pages using YAML configurations.",
+      preview: "https://github.com/Chinmay-03/shorty",
+      techStack: ["JavaScript", "Node.js", "GitHub Pages", "YAML"],
+    },
+    {
+      projectName: "MediBuddy",
+      image: "images/medibuddy.png",
+      summary:"MediBuddy- Built a voice and chat-based bot to assist users with health-related queries, offering disease information and connections to nearby doctors",
+      preview: "https://github.com/chinmay-03",
+      techStack: ["Python"],
+    },
+    
+  ],
+  // Removed androidProjects
+  // Removed freelanceProjects
+};
+
+export const experience = [
+  {
+    title: "Cognifront",
+    duration: "December 23 - January 24",
+    subtitle: "Project Development Intern",
+    details: ["Builing Pandas in JS"],
+    tags: ["Python", "Pandas", "Numpy", "Javascript"],
+    icon: "laptop ",
+  },
+  
+];
+
+export const education = [
+  {
+    title: "B.E. in Artificial Intelligence and Data Science",
+    duration: "2021 - 2025",
+    subtitle: "K.K.Wagh Institute of Engineering, Nashik",
+    details: [],
+    tags: [
+      "Data Structures & Algorithms",
+      "Artifical Intelligence",
+      "Artifical Neural Network",
+      "Machine Learning",
+      "Operating Systems",
+      "Database Management System",
+      "Computer Networks",
+      "Cloud Computing",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Class 12th in Science and Mathematics",
+    duration: "",
+    subtitle: "Board of Higher Secondary Education, Maharashtra",
+    details: [],
+    tags: ["Physics", "Chemistry", "Mathematics","English"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+//   {
+//     label: "Dev Profiles",
+//     data: [
+//       {
+//         text: "Stackoverflow",
+//         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+//       },
+//       {
+//         text: "GitHub",
+//         link: "https://github.com/vinaysomawat",
+//       },
+//       {
+//         text: "LeetCode",
+//         link: "https://leetcode.com/somawatvinay/",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Resources",
+//     data: [
+//       {
+//         text: "Enable Dark/Light Mode",
+//         func: "enableDarkMode()",
+//       },
+//       {
+//         text: "Print this page",
+//         func: "window.print()",
+//       },
+//       {
+//         text: "Clone this page",
+//         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+//       },
+//     ],
+//   },
+  {
+    label: "Links",
+    data: [
+      {
+        text: "Linkedin",
+        link: "https://www.linkedin.com/in/chinmaykotkar/",
+      },
+      {
+        text: "Twitter",
+        link: "https://twitter.com/Chinmay__K8",
+      },
+      // {
+      //   text: "Buy me a coffee",
+      //   link: "https://www.buymeacoffee.com/r194dME8y",
+//       // },
+      {
+        text: "Github",
+        link: "https://github.com/Chinmay-03",
+      },
+    ],
+  },
+  {
+    // label: "copyright-text",
+    // data: [
+    //   "Made with &hearts; by Vinay Somawat.",
+    //   "&copy; No Copyrights. Feel free to use this template.",
+    // ],
+  },
+];
